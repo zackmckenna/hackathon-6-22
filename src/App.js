dd4ad475d02b08ac09ad9a4ed30ff51f99263661
Zack McKenna <zackmckenna@gmail.com>
2020-06-22 12:47:39 -0400

    added temp variable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleAuthClick = () => {
+    fetch('https://api.github.com/login/oath/authorize')
+      .then(res => res.json())
+  }
+
+  useEffect(() => {
+    console.log(process.env.REACT_APP_TEST)
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           MINTBEAN hackathon 6-22
+          <button onClick={() => handleAuthClick()}>AUTH</button>
         </p>
       </header>
     </div>
